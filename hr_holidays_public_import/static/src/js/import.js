331111f153cce4de742ba1b8a1e92da01031212a
Amira Trabelsi <at@transprog.org>
2017-08-30 13:20:59 +0100

    [IMP] Get provider and country data to display on drop box

--- hr_holidays_public_import/static/src/js/import.js
+++ hr_holidays_public_import/static/src/js/import.js
@@ -2,26 +2,34 @@
     var QWeb = openerp.web.qweb;
     _t = instance.web._t;
     
-    //var base_import = instance.base_import
     instance.web.DataImport.include({
+    	get_data: function(model_name, fields) {
+    		var data_list = []
+    		return new instance.web.Model(model_name)
+			.query(fields)
+			.all().
+			then(function(data) {
+					var dd=[];
+					var i;
+					for (i = 0; i < data.length; i++) {
+						dd.push({id : data[i][fields[0]], name : data[i][fields[1]]
+							});
+						}
+						return dd;
+					});
+    	},
     	start: function () {
     		this._super();
     		var self = this;
     		if (this.res_model == "hr.holidays.public") { 
-    			var provider_list = new instance.web.Model('calendar.provider')
-    			.query(['id','provider_name'])
-    			.all().
-    			then(function(data) {
-    					var dd=[];
-    					var i;
-    					for (i = 0; i < data.length; i++) {dd.push({id : data[i].id, name : data[i].provider_name});}
-    						return dd;
-    					}).then(function(result) {self.$el.append(
-    		                    QWeb.render('ImportView.holidays.public', {'providers' : result}
-    		                    ));});
-    			/*this.$el.append(
-                    QWeb.render('ImportView.holidays.public', {'providers' : provider_list}
-));*/
+    			$.when(this.get_data('calendar.provider', ['id', 'provider_name']), this.get_data('res.country', ['id', 'name']))
+    			.done(function(providers, countries) {
+    				self.$el.append(
+    	                    QWeb.render('ImportView.holidays.public', {
+    	                    	'providers' :providers,
+    	                    	'countries' :countries}
+    	                    		));
+    				});
     			};
     	},
     	
